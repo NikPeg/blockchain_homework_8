f151c5adfca83a29b02b1e946eee058754f3bd50
NikPeg <peganov.nik@gmail.com>
2022-12-28 11:15:25 +0300

    Add a contract

---
+++ SimpleContract.sol
@@ -0,0 +1,13 @@
+// SimpleContract.sol
+pragma solidity ^0.4.18;
+contract SimpleContract {
+    uint age;
+    
+    function getAge() returns (uint) {
+        return age;
+    }
+    
+    function setAge(uint newAge) {
+        age = newAge;
+    }
+}
